e2b654436f744cd515e57a4de4f9d6b9136a6752
Tadhg O'Rourke <orourketadhg@gmail.com>
2018-10-28 23:30:28 +0000

    Advanced GUI part 1 - spinner boxes and text windows

--- ModulesAndFunctions/AdvancedGUI.py
+++ ModulesAndFunctions/AdvancedGUI.py
@@ -36,6 +36,7 @@ for zone in os.listdir('C:\Windows\System32'):
 # needs to be oriented, command to
 ListScroll = tkinter.Scrollbar(mainWindow, orient=tkinter.VERTICAL, command=fileList.yview)
 ListScroll.grid(row=1, column=1, sticky='nsw', rowspan=2)
+
 # link scroll bar to listbox
 fileList['yscrollcommand'] = ListScroll.set
 
@@ -60,6 +61,63 @@ radio3 = tkinter.Radiobutton(radioFrame, text="TimeStamp", value=3, variable=rbV
 radio1.grid(row=0, column=0, sticky='w')
 radio2.grid(row=1, column=0, sticky='w')
 radio3.grid(row=2, column=0, sticky='w')
+
+
+
+# Result Box
+resultLabel = tkinter.Label(mainWindow, text="Result")
+resultLabel.grid(row=2, column=2, sticky='nw')
+
+# Text Entry Box
+result = tkinter.Entry(mainWindow)
+result.grid(row=2, column=2, sticky='sw')
+
+
+
+# Time Spinner
+# Frame
+timeFrame = tkinter.LabelFrame(mainWindow, text="Time")
+timeFrame.grid(row=3, column=0, sticky='nsw')
+
+# Time spinner options
+# values/(from_ to) are the numbers able to be chosen in the spinners
+# value will always take add -1 to the value
+hourSpinner = tkinter.Spinbox(timeFrame, width=2, values=tuple(range(0, 24)))
+minuteSpinner = tkinter.Spinbox(timeFrame, width=2, from_=0, to=59)
+secondSpinner = tkinter.Spinbox(timeFrame, width=2, from_=0, to=59)
+
+# Time spinner locations
+hourSpinner.grid(row=0, column=0)
+tkinter.Label(timeFrame, text=':').grid(row=0, column=1)
+minuteSpinner.grid(row=0, column=2)
+tkinter.Label(timeFrame, text=':').grid(row=0, column=3)
+secondSpinner.grid(row=0, column=4)
+
+# adds padding on x axis
+timeFrame['padx'] = 36
+
+
+
+# Date Spinners
+# Frame
+dateFrame = tkinter.LabelFrame(mainWindow, text='Date')
+dateFrame.grid(row=3, column=2, sticky='nsew')
+
+# Spinners
+daySpinner = tkinter.Spinbox(dateFrame, width=2, from_=0, to=31)
+monthSpinner = tkinter.Spinbox(dateFrame, width=5, values=["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"])
+yearSpinner = tkinter.Spinbox(dateFrame, width=4, from_=0, to=9999)
+
+# Date spinner locations
+tkinter.Label(dateFrame, text='Day:').grid(row=0, column=0)
+daySpinner.grid(row=0, column=1)
+tkinter.Label(dateFrame, text='Month:').grid(row=0, column=2)
+monthSpinner.grid(row=0, column=3)
+tkinter.Label(dateFrame, text='Year:').grid(row=0, column=4)
+yearSpinner.grid(row=0, column=5)
+
+dateFrame['padx'] = 25
+
 
 mainWindow.mainloop()
 
