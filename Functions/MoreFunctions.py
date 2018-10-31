7d1125649f5dba5eca524033ac8a281aefcac4f6
Tadhg O'Rourke <orourketadhg@gmail.com>
2018-10-31 15:55:04 +0000

    dealing with scope issues

--- Functions/MoreFunctions.py
+++ Functions/MoreFunctions.py
@@ -8,22 +8,24 @@ def parabola(x):
 
 
 # move origin to center of canvas -- default is left top
-def draw_axis(canvas):
-    # canvas to be updated
-    canvas.update()
+def draw_axis(canvas_template):
+    # canvas_template to be updated
+    canvas_template.update()
     # find new origin points
-    x_origin = canvas.winfo_width() / 2
-    y_origin = canvas.winfo_height() / 2
+    x_origin = canvas_template.winfo_width() / 2
+    y_origin = canvas_template.winfo_height() / 2
     # gets the new outer edge points
-    canvas.configure(scrollregion=(-x_origin, -y_origin, x_origin, y_origin))
+    canvas_template.configure(scrollregion=(-x_origin, -y_origin, x_origin, y_origin))
     # draw lines on zero lines
     # (x1, y1) is the starting point, (x2, y2) is the end point - .create_line(x1, y1, x2, y2), fill="" is the colour
-    canvas.create_line(-x_origin, 0, x_origin, 0, fill="black")
-    canvas.create_line(0, -y_origin, 0, y_origin, fill="black")
+    canvas_template.create_line(-x_origin, 0, x_origin, 0, fill="black")
+    canvas_template.create_line(0, -y_origin, 0, y_origin, fill="black")
+    # prints local variables
+    print(locals())
 
 
-def plot(canvas, x, y):
-    canvas.create_line(x, y, x + 1, y + 1, fill="red")
+def plot(canvas_template, x, y):
+    canvas_template.create_line(x, y, x + 1, y + 1, fill="white")
 
 
 mainWindow = tkinter.Tk()
@@ -31,9 +33,15 @@ mainWindow = tkinter.Tk()
 mainWindow.title("Parabola")
 mainWindow.geometry("640x480")
 
-canvas = tkinter.Canvas(mainWindow, width=640, height=480)
+canvas = tkinter.Canvas(mainWindow, width=320, height=480, background='red' )
 canvas.grid(row=0, column=0)
+
+canvas2 = tkinter.Canvas(mainWindow, width=320, height=480, background='blue')
+canvas2.grid(row=0, column=1)
+
+print(repr(canvas), repr(canvas2))
 draw_axis(canvas)
+draw_axis(canvas2)
 
 
 for i in range(-100, 100):
