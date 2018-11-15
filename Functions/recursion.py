b2ff6280b35eb26ab37011c380527ea5948b1e49
Tadhg O'Rourke <orourketadhg@gmail.com>
2018-11-15 22:30:17 +0000

    nested functions

--- Functions/recursion.py
+++ Functions/recursion.py
@@ -1,4 +1,6 @@
-# generally recursion is not used in python
+import os
+# generally recursion is not used in python,
+# but depending on problem may still be used
 # can effects the processing time due to stacking
 
 
@@ -11,8 +13,8 @@ def fact(n):
     return result
 
 
-for i in range(15):
-    print(i, ': ', fact(i))
+# for i in range(15):
+#     print(i, ': ', fact(i))
 
 
 # factorial(n) using recursion
@@ -23,5 +25,41 @@ def fact_rec(n):
         return n * fact_rec(n - 1)
 
 
-for i in range(15, 0, -1):
-    print(i, ': ', fact_rec(i))
+# for i in range(15, 0, -1):
+#     print(i, ': ', fact_rec(i))
+
+listing = os.walk('.')
+
+
+def list_directories(s):
+
+    # nested function
+    def dir_list(d):
+        # will get a variable from the parent function - like global
+        nonlocal tab_stop
+        files = os.listdir(d)
+        for f in files:
+            current_dir = os.path.join(d, f)
+            if os.path.isdir(current_dir):
+                print("\t" * tab_stop + "Directory " + f)
+                tab_stop += 1
+                dir_list(current_dir)
+                tab_stop -= 1
+            else:
+                print("\t" * tab_stop + f)
+
+    # nested function cannot see out scope variable
+    tab_stop = 0
+    if os.path.exists(s):
+        print("Directory list of " + s)
+        dir_list(s)
+    else:
+        print(s + " does not exist")
+
+
+# for root, directories, files in listing:
+#     print(root)
+#     for d in directories:
+#         print(d)
+#     for file in files:
+#         print(file)
