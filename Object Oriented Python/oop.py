f89120e3585d9fe9dcf5a3c80edf034fa4742376
Tadhg O'Rourke <orourketadhg@gmail.com>
2018-11-19 22:03:46 +0000

    new formating method added some definations of some OOP phrases

--- Object Oriented Python/oop.py
+++ Object Oriented Python/oop.py
@@ -20,7 +20,7 @@ class Kettle(object):
     # initialising passed variables common to all methods in a class
     # self used to access other attributes in a method
     # all other attributes must be initialised in order to be used in the class
-    def __init__(self, make, price):
+    def __init__(self, make, price=100.00):
         # assigning a attribute to self.[name] so it can be used in the class by referring to self.[name]
         self.make = make
         self.price = price
@@ -28,6 +28,7 @@ class Kettle(object):
 
 
 Kenwood = Kettle("Kenwood", 120.00)
+hamilton = Kettle("Hamilton")
 # . notation is used to access things in a object
 print(Kenwood.price)
 print(Kenwood.make)
@@ -35,3 +36,15 @@ print(Kenwood.make)
 # overwrites object variable price
 Kenwood.price = 119.99
 print(Kenwood.price)
+
+# can format the replacement fields to hold the class .attribute data
+print('Models: {0.make}: {0.price}, {1.make}: {1.price}'.format(Kenwood, hamilton))
+
+"""
+Class : template of an object
+Object : Instance of a class
+Instantiate : Create instance of a class
+Method : A function inside a class
+Attribute : a variable found in an instance of a class
+"""
+
