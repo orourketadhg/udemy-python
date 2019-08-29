4752fdfd8d196d6e01435806e05a030f2c0c4de4
Tadhg O'Rourke <orourketadhg@gmail.com>
2019-08-29 19:46:15 +0100

    starting to look at ways to prevent sql injection attacks

--- Databases/Contacts2.py
+++ Databases/Contacts2.py
@@ -13,13 +13,24 @@ import sqlite3
 # create connection to db
 db = sqlite3.connect("contacts.sqlite")
 
+new_email = "Bleep@email.com"
+new_phone = input("phone Number: ")
+
 # run update
-update_sql = "UPDATE contacts SET email = 'test@myemail.ie' WHERE email LIKE 'test@email.com'"
+# update_sql = "UPDATE contacts SET email = '{}' WHERE phone == {}".format(new_email, new_phone)
+
+# to attempt to stop sql injection attacks
+update_sql = "UPDATE contacts SET email = ? WHERE phone == ?"
 update_cursor = db.cursor()
-update_cursor.execute(update_sql)
+
+# will run multiple statements at once - separated by semicolons
+# extra parameters will replace the question marks in the sql statement - will parse to stop injection attacks
+update_cursor.execute(update_sql, (new_email, new_phone))
 
 # commit update
 update_cursor.connection.commit()
+
+update_cursor.close()
 
 # get count of rows affected
 print(update_cursor.rowcount)
@@ -27,5 +38,5 @@ print(update_cursor.rowcount)
 for row in db.execute("SELECT * FROM Contacts"):
     print(row)
 
-update_cursor.close()
+
 db.close()
