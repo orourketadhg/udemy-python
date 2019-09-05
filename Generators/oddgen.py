7fbc27ab77e222bf9ac0719e8177bb58b2cd5ae7
Tadhg O'Rourke <orourketadhg@gmail.com>
2019-09-05 18:25:15 +0100

    infinite pi generator

--- Generators/oddgen.py
+++ Generators/oddgen.py
@@ -10,12 +10,33 @@ __date__ = "05/09/2019"
 
 
 # generates n odd numbers
-def odd_gen(n: int):
+def odd_gen():
+# def odd_gen(n: int):
     current = 1
-    while current <= n:
+
+    # while current <= n:
+    while True:
         yield current
         current += 2
 
 
-for i in odd_gen(100):
-    print(i)
+# for i in odd_gen():
+#     print(i)
+
+
+def pi_series():
+    odds = odd_gen()
+    approx = 0
+    while True:
+
+        approx += (4 / next(odds))
+        yield approx
+
+        approx -= (4 / next(odds))
+        yield approx
+
+
+approx_pi = pi_series()
+
+for x in range(10000):
+    print(next(approx_pi))
