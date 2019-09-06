9305ac609ea161ef7b918f0425d7c54ed1e4dd60
Tadhg O'Rourke <orourketadhg@gmail.com>
2019-09-06 16:17:52 +0100

    conditional expressions

--- ListComprehensions/conditional_comprehensions.py
+++ ListComprehensions/conditional_comprehensions.py
@@ -23,7 +23,7 @@ for meal in menu:
     if "spam" not in meal:
         meals.append(meal)
     else:
-        meals.append("meal skipped")
+        meals.append("a meal")
 print(meals)
 
 # conditional comprehension
@@ -32,6 +32,49 @@ print(meals)
 
 # do the same thing - choose which is more readable for filter
 # meals = [meal for meal in menu if "spam" not in meal if "chicken" not in meal]
-meals = [meal for meal in menu if "spam" not in meal and "chicken" not in meal]
+# meals = [meal for meal in menu if "spam" not in meal and "chicken" not in meal]
 
+
+# conditional comprehension using a conditional expression
+# like a conditional comprehension - allows for more complexity
+
+# [ conditional expression | iterator ]
+meals = [meal if "spam" not in meal else "a meal" for meal in menu]
 print(meals)
+
+print("=" * 40)
+
+x = 12
+
+# conditional expression - conditional on a single line
+expression = "Twelve" if x == 12 else "unknown"
+
+print(expression)
+print("=" * 40)
+
+for meal in menu:
+    # complex conditional expression - unclear and contains bugs
+    print(meal, "contains sausage" if "sausage" in meal else "contains bacon" if "bacon" in meal else "contains egg")
+
+print("=" * 40)
+
+
+items = set()
+for meal in menu:
+    for item in meal:
+        items.add(item)
+print(items)
+
+print("=" * 40)
+
+for meal in menu:
+    for item in meal:
+        if item in meal:
+            print("{} contains {}".format(meal, item))
+            break
+
+print("=" * 40)
+
+for x in range(1, 31):
+    fizzbuzz = "fizz buzz" if x % 15 == 0 else "fizz" if x % 3 == 0 else "buzz" if x % 5 == 0 else str(x)
+    print(fizzbuzz)
